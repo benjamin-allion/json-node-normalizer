8bc9ac88b80e771a79bcd0a0a221c066f317f615
Benjamin ALLION <benjamin.allion.pro@gmail.com>
2019-06-04 21:38:45 +0200

    tmp(logs): temporary force 'debug' log level.

--- config/logger.js
+++ config/logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-  level: process.env.LOG_LEVEL,
+  level: 'debug',
   format: format.json(),
   transports: [
     new transports.Console(),
