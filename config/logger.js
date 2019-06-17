c4d4148cc1660009eea1c57c5c8153f9644c1fe3
Benjamin ALLION <benjamin.allion.pro@gmail.com>
2019-06-17 13:57:20 +0200

    feat(logs): new 'JSON_NODE_NORMALIZER_LOGGING_LEVEL' env. variable.

--- config/logger.js
+++ config/logger.js
@@ -1,7 +1,7 @@
 const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
-  level: 'debug',
+  level: process.env.JSON_NODE_NORMALIZER_LOGGING_LEVEL || 'info',
   format: format.json(),
   transports: [
     new transports.Console(),
