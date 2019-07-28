d93e91389185abf9945b597b0218d4437339b65e
ALLION Benjamin <benjamin.allion.pro@gmail.com>
2019-07-28 10:32:59 +0200

    test(format): 'LOWERCASE_FORMAT' formatting test. ref #15.

--- tests/unit/lib/core/formats.spec.js
+++ tests/unit/lib/core/formats.spec.js
@@ -13,4 +13,17 @@ describe('formats.js', () => {
     const expectedResult = 'TEST';
     expect(normalizedNode).toBe(expectedResult);
   });
+
+  it('try to normalize json node to \'LOWERCASE_FORMAT\'', async () => {
+    // Given
+    const jsonNode = 'TEST';
+    const targetFormat = FormatTypes.LOWERCASE_FORMAT;
+
+    // When
+    const normalizedNode = normalizeFormat(jsonNode, targetFormat);
+
+    // Then
+    const expectedResult = 'test';
+    expect(normalizedNode).toBe(expectedResult);
+  });
 });
