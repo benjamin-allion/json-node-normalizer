dc5d9609e24a21185b2c83eb7d944c79cca82275
ALLION Benjamin <benjamin.allion.pro@gmail.com>
2019-10-20 13:09:19 +0200

    test(types): increase NUMBER_TYPE normalization tests for decimal. ref #24.

--- tests/unit/lib/core/types/number.spec.js
+++ tests/unit/lib/core/types/number.spec.js
@@ -1,5 +1,5 @@
-const JsonNodeNormalizer = require('../../../../index');
-const { NodeTypes } = require('../../../../lib/core/types');
+const JsonNodeNormalizer = require('../../../../../index');
+const { NodeTypes } = require('../../../../../lib/core/types');
 
 describe('types.js', () => {
   it(`must convert 'String' to 'Number'`, () => {
@@ -30,5 +30,25 @@ describe('types.js', () => {
     jsonToNormalize = JsonNodeNormalizer.normalizePath(jsonToNormalize, 'fieldToNormalize', targetType);
     // Then
     expect(jsonToNormalize).toEqual({ fieldToNormalize: 123.23 });
+  });
+
+  it(`must keep correct number value`, () => {
+    // Given
+    let jsonToNormalize = { fieldToNormalize: 123 };
+    const targetType = NodeTypes.NUMBER_TYPE;
+    // When
+    jsonToNormalize = JsonNodeNormalizer.normalizePath(jsonToNormalize, 'fieldToNormalize', targetType);
+    // Then
+    expect(jsonToNormalize).toEqual({ fieldToNormalize: 123 });
+  });
+
+  it(`must keep correct number value (Decimal support)`, () => {
+    // Given
+    let jsonToNormalize = { fieldToNormalize: 123.123 };
+    const targetType = NodeTypes.NUMBER_TYPE;
+    // When
+    jsonToNormalize = JsonNodeNormalizer.normalizePath(jsonToNormalize, 'fieldToNormalize', targetType);
+    // Then
+    expect(jsonToNormalize).toEqual({ fieldToNormalize: 123.123 });
   });
 });
