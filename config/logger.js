0bd125a9f9e826d117d7333e897db5b2394a4be6
ALLION Benjamin <benjamin.allion.pro@gmail.com>
2021-11-26 19:23:09 +0100

    refactor(lint): fix lint problems.

--- config/logger.js
+++ config/logger.js
@@ -2,9 +2,9 @@ const { createLogger, format, transports } = require('winston');
 
 let logger = {
   debug: () => {}
-}
+};
 
-if(process.env.JSON_NODE_NORMALIZER_DEBUG?.toLowerCase() === 'true') {
+if (process.env.JSON_NODE_NORMALIZER_DEBUG?.toLowerCase() === 'true') {
   logger = createLogger({
     level: process.env.JSON_NODE_NORMALIZER_LOGGING_LEVEL || 'info',
     format: format.json(),
--- tests/unit/lib/core/schemaCache.spec.js
+++ tests/unit/lib/core/schemaCache.spec.js
@@ -1,4 +1,5 @@
-const SchemaCache = require('../../../../lib/core/schemaCache')
+/* eslint-disable no-await-in-loop */
+const SchemaCache = require('../../../../lib/core/schemaCache');
 const { FormatTypes } = require('../../../../lib/core/formats');
 const JsonNodeNormalizer = require('../../../../index');
 
@@ -7,153 +8,152 @@ const JsonNodeNormalizer = require('../../../../index');
  * @param {number} ms
  * @returns {Promise}
  */
-const sleepy = ms => new Promise(resolve => setTimeout(() => resolve(), ms))
+const sleepy = ms => new Promise(resolve => { setTimeout(() => resolve(), ms); });
 
 describe('schemaCache.js', () => {
-    it('should store object data into the cache', async () => {
-        // Given
-        const objectToStore = {
-            test: "OK"
+  it('should store object data into the cache', async () => {
+    // Given
+    const objectToStore = {
+      test: "OK"
+    };
+
+    // When
+    SchemaCache.setData('objectTest', objectToStore, 200);
+    SchemaCache.setData('objectTest', objectToStore, 20); // Second setData should be ignored
+    await sleepy(100);
+
+    // Then
+    expect(SchemaCache.getData('objectTest')).toEqual(objectToStore);
+    await sleepy(200); // Wait for cache clear
+  });
+
+  it('should remove object from cache after delay', async () => {
+    // Given
+    const objectToStore = {
+      test: "OK"
+    };
+
+    // When
+    SchemaCache.setData('objectTest', objectToStore, 500);
+    await sleepy(600);
+
+    // Then
+    expect(SchemaCache.getData('objectTest')).toEqual(undefined);
+  });
+
+  it(`'fieldsToNormalize' should be store & removed from the cache`, async () => {
+    // Given
+    const cacheSpy = jest.spyOn(SchemaCache, 'setData');
+    const jsonData = {
+      data: {
+        enable: 'true',
+        lastName: 'must_be_uppercase',
+        firstName: 'MUST_BE_LOWERCASE',
+      }
+    };
+    const jsonSchema = {
+      schemaName: "1233487",
+      data: {
+        type: 'object',
+        properties: {
+          enable: {
+            type: 'boolean'
+          },
+          lastName: {
+            type: 'string',
+            format: FormatTypes.UPPERCASE
+          },
+          firstName: {
+            type: 'string',
+            format: FormatTypes.LOWERCASE
+          },
         }
+      }
+    };
+    const customConfig = {
+      useCache: true,
+      cacheId: "schemaName",
+      cacheDuration: 800
+    };
 
-        // When
-        SchemaCache.setData('objectTest', objectToStore, 200)
-        SchemaCache.setData('objectTest', objectToStore, 20) // Second setData should be ignored
-        await sleepy(100)
+    // When
+    let result;
+    for (let i = 0; i < 5; i += 1) {
+      result = await JsonNodeNormalizer.normalize(jsonData, jsonSchema, customConfig);
+    }
+    await sleepy(900); // Invalid cache
 
-        // Then
-        expect(SchemaCache.getData('objectTest')).toEqual(objectToStore)
-        await sleepy(200) // Wait for cache clear
-    });
+    for (let i = 0; i < 5; i += 1) {
+      result = await JsonNodeNormalizer.normalize(jsonData, jsonSchema, customConfig);
+    }
 
-    it('should remove object from cache after delay', async () => {
-        // Given
-        const objectToStore = {
-            test: "OK"
+    // Then
+    const expectedResult = {
+      data: {
+        enable: true,
+        lastName: 'MUST_BE_UPPERCASE',
+        firstName: 'must_be_lowercase',
+      }
+    };
+    expect(result).toStrictEqual(expectedResult);
+    expect(cacheSpy).toHaveBeenCalledTimes(2);
+    await sleepy(900); // Wait for cache clear
+    jest.clearAllMocks();
+  });
+
+  it(`Should clear cache between each calls`, async () => {
+    // Given
+    const cacheSpy = jest.spyOn(SchemaCache, 'setData');
+    const jsonData = {
+      data: {
+        enable: 'true',
+        lastName: 'must_be_uppercase',
+        firstName: 'MUST_BE_LOWERCASE',
+      }
+    };
+    const jsonSchema = {
+      schemaName: "1233489",
+      data: {
+        type: 'object',
+        properties: {
+          enable: {
+            type: 'boolean'
+          },
+          lastName: {
+            type: 'string',
+            format: FormatTypes.UPPERCASE
+          },
+          firstName: {
+            type: 'string',
+            format: FormatTypes.LOWERCASE
+          },
         }
+      }
+    };
+    const customConfig = {
+      useCache: true,
+      cacheId: "schemaName",
+      cacheDuration: 200
+    };
 
-        // When
-        SchemaCache.setData('objectTest', objectToStore, 500)
-        await sleepy(600)
+    // When
+    let result;
+    for (let i = 0; i < 5; i += 1) {
+      result = await JsonNodeNormalizer.normalize(jsonData, jsonSchema, customConfig);
+      JsonNodeNormalizer.clearCache();
+    }
 
-        // Then
-        expect(SchemaCache.getData('objectTest')).toEqual(undefined)
-    });
-
-    it(`'fieldsToNormalize' should be store & removed from the cache`, async () => {
-        // Given
-        const cacheSpy = jest.spyOn(SchemaCache, 'setData');
-        const jsonData = {
-            data: {
-                enable: 'true',
-                lastName: 'must_be_uppercase',
-                firstName: 'MUST_BE_LOWERCASE',
-            }
-        };
-        const jsonSchema = {
-            schemaName: "1233487",
-            data: {
-                type: 'object',
-                properties: {
-                    enable: {
-                        type: 'boolean'
-                    },
-                    lastName: {
-                        type: 'string',
-                        format: FormatTypes.UPPERCASE
-                    },
-                    firstName: {
-                        type: 'string',
-                        format: FormatTypes.LOWERCASE
-                    },
-                }
-            }
-        };
-        const customConfig = {
-            useCache: true,
-            cacheId: "schemaName",
-            cacheDuration: 800
-        }
-
-        // When
-        let result;
-        for(let i=0; i<5; i++) {
-            result = await JsonNodeNormalizer.normalize(jsonData, jsonSchema, customConfig);
-        }
-        await sleepy(900) // Invalid cache
-
-        for(let i=0; i<5; i++) {
-            result = await JsonNodeNormalizer.normalize(jsonData, jsonSchema, customConfig);
-        }
-
-
-        // Then
-        const expectedResult = {
-            data: {
-                enable: true,
-                lastName: 'MUST_BE_UPPERCASE',
-                firstName: 'must_be_lowercase',
-            }
-        };
-        expect(result).toStrictEqual(expectedResult);
-        expect(cacheSpy).toHaveBeenCalledTimes(2);
-        await sleepy(900) // Wait for cache clear
-        jest.clearAllMocks();
-    });
-
-    it(`Should clear cache between each calls`, async () => {
-        // Given
-        const cacheSpy = jest.spyOn(SchemaCache, 'setData');
-        const jsonData = {
-            data: {
-                enable: 'true',
-                lastName: 'must_be_uppercase',
-                firstName: 'MUST_BE_LOWERCASE',
-            }
-        };
-        const jsonSchema = {
-            schemaName: "1233489",
-            data: {
-                type: 'object',
-                properties: {
-                    enable: {
-                        type: 'boolean'
-                    },
-                    lastName: {
-                        type: 'string',
-                        format: FormatTypes.UPPERCASE
-                    },
-                    firstName: {
-                        type: 'string',
-                        format: FormatTypes.LOWERCASE
-                    },
-                }
-            }
-        };
-        const customConfig = {
-            useCache: true,
-            cacheId: "schemaName",
-            cacheDuration: 200
-        }
-
-        // When
-        let result;
-        for(let i=0; i<5; i++) {
-            result = await JsonNodeNormalizer.normalize(jsonData, jsonSchema, customConfig);
-            JsonNodeNormalizer.clearCache();
-        }
-
-        // Then
-        const expectedResult = {
-            data: {
-                enable: true,
-                lastName: 'MUST_BE_UPPERCASE',
-                firstName: 'must_be_lowercase',
-            }
-        };
-        expect(result).toStrictEqual(expectedResult);
-        expect(cacheSpy).toHaveBeenCalledTimes(5);
-        await sleepy(200) // Wait for cache clear
-        jest.clearAllMocks();
-    });
+    // Then
+    const expectedResult = {
+      data: {
+        enable: true,
+        lastName: 'MUST_BE_UPPERCASE',
+        firstName: 'must_be_lowercase',
+      }
+    };
+    expect(result).toStrictEqual(expectedResult);
+    expect(cacheSpy).toHaveBeenCalledTimes(5);
+    await sleepy(200); // Wait for cache clear
+    jest.clearAllMocks();
+  });
 });
